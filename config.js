73dd9f975a53ab887775c0c145ceb5fdcd0601e6
safsal213 <haay.yo@gmail.com>
2026-07-28 10:10:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   // בשלב הבא נחבר כאן כתובת Google Apps Script שמחזירה JSON.
   // כרגע משאירים ריק והמערכת משתמשת בנתוני הדוגמה המקומיים.
-  DATA_URL: "",
+  DATA_URL: "https://script.google.com/macros/s/AKfycbwVfXfBJfQKDDIA9lm0BQ2mGfeYQArbP-AfKsbU4GDkSPsFc9hTnKvS4c9yX9_NqtZEPA/exec",
 
   // זמן ברירת מחדל לכל שקופית בשניות.
   DEFAULT_SLIDE_SECONDS: 10,
